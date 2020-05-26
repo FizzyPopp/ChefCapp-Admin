2710bd05269f2856f294a053074801e98c3f7196
James Liu <liujames.q@gmail.com>
2020-05-26 17:32:20 -0400

    Schema loading, successful packaging, initial testing pipeline

--- cc-admin/index.js
+++ cc-admin/index.js
@@ -2,15 +2,52 @@
  * @module cc-admin
  */
 
+const schemaRegExp = /\.schema\.json$/;
 
 /**
  *  The modules required from Firebase
  */
-var cc = require('firebase-admin')
-var functions = require('firebase-functions')
+var cc = require('firebase-admin');
+var functions = require('firebase-functions');
+var fs = require('fs');
+var path = require('path');
+var Ajv = require('ajv');
+var uuid = require('uuid')
 
-var fs = require('fs')
-var path = require('path')
+
+/**
+ * List of data schemas which defines the data structures
+ * @constant
+ */
+var _schemas = {}
+/**
+ * Load schemas into schema object that match the schema regex
+ */
+fs.readdir(__dirname, (err, files) => {
+    if(err != null) {
+        console.log(err);
+        return;
+    }
+    files.forEach ((file) => {
+        if (file.match(schemaRegExp)) {
+            let uri = 'cc-admin/' + file;
+            console.log('loading schema: ' + uri);
+            var s = require(uri);
+            _schemas[s.title] = s;
+        }
+    });
+});
+exports.schemas = _schemas;
+
+let _ajv = new Ajv();
+
+_ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+for (let name in _schemas) {
+    console.log(_schemas[name]);
+    _ajv.addSchema(_schemas[name], name);
+}
+exports.ajv = _ajv;
+
 
 /**
  * Name of module
@@ -27,8 +64,9 @@ exports.name = 'cc-admin';
 const _app = cc.initializeApp({
     credential: cc.credential.applicationDefault(),
     databaseURL: "https://chef-capp.firebaseio.com"
-})
+});
 exports.app = _app;
+
 
 
 /**
@@ -38,6 +76,7 @@ exports.app = _app;
  */
 const _db = cc.firestore();
 exports.db = _db;
+
 
 
 /** @function test
@@ -55,7 +94,7 @@ exports.test = function (collectionRef) {
         console.log('Error getting documents', err);
     });
     return allDocs;
-}
+};
 
 /** @function getCollection
  * Returns collection from the CC database by name, takes Strings only
@@ -69,25 +108,16 @@ exports.getCollection = function (colName) {
                       });
     }
     throw "Collection name must be a string.";
-}
+};
 
 
 /** @function recipeParse
  * Takes a recipe according to schema and parses it into a Firestore DB element
  * @exports
  */
-exports.recipeParse = function (recipe_string) {
-    if (typeof(recipe_string) === 'string') {
-        //try (JSON.parse(recipe_string)
-    }
+exports.recipeParse = async function (candidate) {
 
-    if (exports.recipeVerify(recipe)) {
-        return recipe
-    }
-    else { throw 'Bad recipe format!'; }
-
-
-}
+};
 
 /**
  * @func recipeVerify
@@ -96,28 +126,13 @@ exports.recipeParse = function (recipe_string) {
  *
  * Returns true if [recipe] contains the fields required
  */
-exports.recipeVerify = function (recipe) {
-    if (
-        recipe.steps === undefined ||
-        recipe.title === undefined ||
-        recipe.number_of_steps === undefined ||
-        recipe.ingredients === undefined
-    ) { return false }
+exports.candidateVerify = function (candidate) {
+    let isRecipe = false
+    let recipeSchema = exports.recipeSchema;
+    for (let required in recipeSchema.required) {
+        if (candidate[required] == null) {
 
-    if (
-        typeof(recipe.title) !== 'string' ||
-        typeof(recipe.number_of_steps) !== 'string'
-    )
-
-    for (const step in recipe.steps) {
-        if (
-            step.num === undefined ||
-            time_min === undefined ||
-            time_max === undefined ||
-            time_avg === undefined ||
-            text === undefined
-        ) { return false }
+        }
     }
-
     return isRecipe;
-}
+};
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/home/james/tooling/chef-capp-firebase-adminsdk-qm4l4-0fed2225d7.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/path/to/your/service-account-file.json"
 export FIREBASE_CONFIG={}
 export GCLOUD_PROJECT={}
