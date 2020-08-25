a8fa5d8ad7bb048eee522a07bac2ab64ec3fb775
James Liu <liujames.q@gmail.com>
2020-08-24 23:12:04 -0400

    added some scripts, moved init - did an oopsie

--- cc-admin/lib/parser.js
+++ cc-admin/lib/parser.js
@@ -1,9 +1,10 @@
 var unified = require('unified');
 var remark = require('remark-parse');
-var copyToken = require('./parser/remark-copy-token');
+var recipeStep = require('./parser/inline/recipe-step');
+
 
 var parser = unified()
   .use(remark, {commonmark: true})
-  .use(copyToken)
+  .use(recipeStep)
 
 module.exports = parser;
